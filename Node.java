45ce1e07784d6eb4604e4c4cc181aeebe0035766
Ethan Trantalis <ethantrantalis@gmail.com>
2023-05-02 14:47:20 -0700

    changed node class name

---
+++ Node.java
@@ -0,0 +1,14 @@
+public class Node {
+
+    private Node next = null;
+
+    private int key = 0;
+
+    public Node(Node next, int key){
+        this.next = next;
+        this.key = key;
+
+    }
+
+
+}
